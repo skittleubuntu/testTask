99cf67aefce03210b1fa5dee1461e1fe34e285b3
Skittle <81906603+skittleubuntu@users.noreply.github.com>
2025-06-09 13:49:18 +0200

    Update tg_client.py

--- tg_client.py
+++ tg_client.py
@@ -16,26 +16,26 @@ def get_dialogs(limit=10):
 #returns a list of up to 100 messages in the dialog,
 #or stops when it reaches a message older than 30 days
 #limit and days can be changed
-def get_messages(dialog, days_back=30, limit=100):
+def get_messages(dialog, days_limit=30, limit=100):
 
     #last possibly day
-    since_date = datetime.now(timezone.utc) - timedelta(days=days_back)
+    last_day = datetime.now(timezone.utc) - timedelta(days=days_limit)
 
     messages = []
 
     #chek every message's in dialog
     for message in client.iter_messages(dialog, limit=limit, reverse=False):
         #if older than 30 days -> break
-        if message.date < since_date:
+        if message.date < last_day:
             break
         #if it's a picture or voice message -> skip
         if not message.text:
             continue
 
-        #every message save in dict format
+
         messages.append({
             "date": message.date.isoformat(),
-            "sender_id": message.sender_id,
+            "senderID": message.sender_id,
             "text": message.text
         })
 
